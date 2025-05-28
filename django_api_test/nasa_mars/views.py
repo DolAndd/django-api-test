64045f988657a9cf9bcd70631656feee16d72341
DolAndd <dolganov2002@mail.ru>
2025-05-28 13:43:31 +0300

    creation nasa_mars app

---
+++ django_api_test/nasa_mars/views.py
@@ -0,0 +1,31 @@
+import os
+import requests
+from django.shortcuts import render
+from dotenv import load_dotenv
+from .models import City
+
+load_dotenv()
+
+
+def weather_view(request):
+    api_key = os.getenv('OPENWEATHER_API_KEY')
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city.name}&units=metric&appid={api_key}'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            weather = {
+                'city': city.name,
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'],
+                'icon': data['weather'][0]['icon'],
+            }
+            weather_data.append(weather)
+
+    context = {'weather_data': weather_data}
+    return render(request, 'weather/weather.html', context)
--- django_api_test/urls.py
+++ django_api_test/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_api_test.nasa_mars import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.weather_view, name='weather'),
 ]
