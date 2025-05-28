d0407252736226b118fc532b149d8479ce3046ce
DolAndd <dolganov2002@mail.ru>
2025-05-28 16:18:38 +0300

    creation weather app

--- django_api_test/settings.py
+++ django_api_test/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_api_test',
     'django_api_test.nasa_mars',
+    'django_api_test.weather',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'django_api_test.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django_api_test/urls.py
+++ django_api_test/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django_api_test.nasa_mars import views
+from django_api_test.weather import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
